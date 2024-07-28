da9c2759e007c67d272a64692e020cb8eeb5cab7
Minespeed1009 <jye.haddon@gmail.com>
2024-07-28 14:26:07 +1000

    maybe fixed modmenu stuff

--- src/main/java/com/pulsar/soulforge/config/SoulForgeConfig.java
+++ src/main/java/com/pulsar/soulforge/config/SoulForgeConfig.java
@@ -1,15 +1,31 @@
 package com.pulsar.soulforge.config;
 
-import com.terraformersmc.modmenu.config.option.EnumConfigOption;
+import com.mojang.serialization.Codec;
+import com.terraformersmc.modmenu.config.option.ConfigOptionStorage;
 import com.terraformersmc.modmenu.config.option.OptionConvertable;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SoulForgeConfig {
-    public static final EnumConfigOption<MagicBarLocation> MAGIC_BAR_LOCATION = new EnumConfigOption<>("magic_bar_location", MagicBarLocation.BOTTOM_LEFT);
+    public static final SimpleOption<MagicBarLocation> MAGIC_BAR_LOCATION = new SimpleOption<>("option.modmenu.magic_bar_location",
+            SimpleOption.emptyTooltip(), (text, value) -> Text.translatable("option.modmenu.magic_bar_location." + value),
+            new SimpleOption.PotentialValuesBasedCallbacks<>(Arrays.asList(MagicBarLocation.class.getEnumConstants()),
+                    Codec.STRING.xmap(
+                            string -> Arrays.stream(MagicBarLocation.class.getEnumConstants()).filter(e -> e.name().toLowerCase().equals(string)).findAny().orElse(null),
+                            newValue -> newValue.name().toLowerCase()
+                    )),
+            MagicBarLocation.BOTTOM_LEFT, value -> {
+                if (FabricLoader.getInstance().isModLoaded("modmenu")) {
+                    ConfigOptionStorage.setEnum("magic_bar_location", value);
+                }
+            }
+    );
     //public static final BooleanConfigOption DEBUG_RESET_PERCENTAGES = new BooleanConfigOption("debug_reset_percentages", false);
 
     public static SimpleOption<?>[] asOptions() {
